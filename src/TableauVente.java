7d4a6d692dba1709e003c591525394fa89f61156
angelique-df <faye.angelique.d@gmail.com>
2023-01-26 16:11:02 +0000

    Add `calculCAParVendeur()`, print array

--- src/TableauVente.java
+++ src/TableauVente.java
@@ -10,7 +10,24 @@ public class TableauVente {
         for (int i = 0; i < ventesParModele.length; i++) {
             System.out.println("Modèle " + (i + 1) + " : " + ventesParModele[i] + " exemplaires vendus.");
         }
+        int[] prixModeles = {1, 2, 3, 4};
+        int[] caParVendeur = calculCAParVendeur(ventes, prixModeles);
+        System.out.println("Chiffres d'affaires par vendeur : ");
+        for (int i = 0; i < caParVendeur.length; i++) {
+            System.out.println("Vendeur " + (i + 1) + " : " + caParVendeur[i]);
+        }
     }
+
+    public static int[] calculCAParVendeur(int[][] ventes, int[] prixModeles) {
+        int[] caParVendeur = new int[ventes[0].length];
+        for (int i = 0; i < ventes[0].length; i++) {
+            for (int j = 0; j < ventes.length; j++) {
+                caParVendeur[i] += ventes[j][i] * prixModeles[j];
+            }
+        }
+        return caParVendeur;
+    }
+
     public static int[] calculVentesParModele(int[][] ventes) {
         int[] ventesParModele = new int[ventes.length];
         for (int i = 0; i < ventes.length; i++) {
